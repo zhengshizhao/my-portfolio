ecde08dcd05c5918025cdf31931f911af6853daf
zhengshizhao <zhao.zhengshi@gmail.com>
2016-05-02 12:41:09 -0400

    add skill-set and CVdownload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Zhengshi Zhao' });
+  res.render('index');
 });
 
 module.exports = router;
